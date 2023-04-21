e20198b28a546a76253c4e51b0b9f517bac7e4e6
Jonah0705 <jonahpupella@gmail.com>
2023-04-21 14:53:28 +0700

    CameraFollowFix
    Fixed Camera Follow script

--- Final GameDev/Assets/CameraFollowPlayer.cs
+++ Final GameDev/Assets/CameraFollowPlayer.cs
@@ -7,6 +7,7 @@ public class CameraFollowPlayer : MonoBehaviour
     public float followSpeed = 2f;
     public float yOffset = 4.3f; //if we want to follow the player height while jumping, unused
     public float xOffset = 10f;
+    public float zOffset = -15f;
     public Transform target;
 
     // Start is called before the first frame update
@@ -18,7 +19,7 @@ public class CameraFollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x + xOffset, 3f ,-12f);
+        Vector3 newPos = new Vector3(target.position.x + xOffset, 3f , zOffset);
         transform.position = Vector3.Slerp(transform.position,newPos,followSpeed*Time.deltaTime);
     }
 }
