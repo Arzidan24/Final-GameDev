0197457ae870b3b3cccb41f5108e8533ee9d2829
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2023-05-07 00:51:45 +0700

    camer

--- Final GameDev/Assets/CameraFollowPlayer.cs
+++ Final GameDev/Assets/CameraFollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollowPlayer : MonoBehaviour
 {
     public float followSpeed = 2f;
-    public float yOffset = 5f; //if we want to follow the player height while jumping, unused
+    public float yOffset = 1f; //if we want to follow the player height while jumping, unused
     public float xOffset = 10f;
     public float zOffset = -15f;
     public Transform target;
@@ -19,7 +19,7 @@ public class CameraFollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x + xOffset, 3f , zOffset);
+        Vector3 newPos = new Vector3(target.position.x + xOffset, yOffset , zOffset);
         transform.position = Vector3.Slerp(transform.position,newPos,followSpeed*Time.deltaTime);
     }
 }
