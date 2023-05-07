97e66c1f1b7e0582a05c73fdaad5e6cc58be3f9d
GeraldSutarso <92673939+GeraldSutarso@users.noreply.github.com>
2023-05-07 20:10:51 +0700

    coin cling cling

--- Final GameDev/Assets/ItemCollector.cs
+++ Final GameDev/Assets/ItemCollector.cs
@@ -6,11 +6,13 @@ using TMPro;
 public class ItemCollector : MonoBehaviour
 {
     private int coins = 0;
+    public AudioSource coinSFX;
     [SerializeField] private TMP_Text CoinsCollected;
    private void OnTriggerEnter2D(Collider2D collison) {
     if (collison.gameObject.CompareTag("Coin"))
     {
         Destroy(collison.gameObject);
+        coinSFX.Play();
         coins++;
         CoinsCollected.text = "Coins Collected: " + coins;
     }
